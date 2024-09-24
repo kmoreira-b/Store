36cea80e440fb4220cbfc83cc7ca4c9dcaa7ea1b
kmoreira-b <kmoreirab@ucenfotec.ac.cr>
2024-09-24 11:01:06 -0600

    Crud productos y ordenes

--- src/StoreSQL.java
+++ src/StoreSQL.java
@@ -79,8 +79,9 @@ public class StoreSQL {
         while (loggedIn) {
             out.println("¿Qué desea hacer?");
             out.println("1 - Manejar usuarios");
-            out.println("2 - Realizar compra (No implementado)");
-            out.println("3 - Salir y cerrar sesión");
+            out.println("2 - Manjear prodcutos");
+            out.println("3 - Manjear ordenes");
+            out.println("4 - Salir y cerrar sesión");
 
             int choice = getUserChoice();
             switch (choice) {
@@ -88,9 +89,12 @@ public class StoreSQL {
                     manageUsers();
                     break;
                 case 2:
-                    out.println("Esta opción aún no está implementada.");
+                    manageProducts();
                     break;
                 case 3:
+                    manageOrders();
+                    break;
+                case 4:
                     out.println("Cerrando sesión...");
                     loggedIn = false; // Exit back to main menu
                     break;
@@ -246,5 +250,408 @@ public class StoreSQL {
 
     private static void register() {
         createUser();
+    }
+
+    private static void manageProducts() throws IOException{
+        boolean managingProducts = true;
+
+        while (managingProducts) {
+            out.println("Gestión de Productos:");
+            out.println("1 - Crear Producto");
+            out.println("2 - Listar Productos");
+            out.println("3 - Actualizar Producto");
+            out.println("4 - Eliminar Producto");
+            out.println("5 - Agregar productos a favoritos");
+            out.println("6 - Listar productos favoritos");
+            out.println("7 - Eliminar producto de favoritos");
+            out.println("8 - Regresar al menú anterior");
+
+            int choice = getUserChoice();
+            switch (choice) {
+                case 1:
+                    createProducts();
+                    break;
+                case 2:
+                    listProducts();
+                    break;
+                case 3:
+                    updateProducts();
+                    break;
+                case 4:
+                    deleteProducts();
+                    break;
+                case 5:
+                    wishlist();
+                    break;
+                case 6:
+                    listarWishlist();
+                    break;
+                case 7:
+                    deleteWishlist();
+                    break;
+                case 8:
+                    managingProducts = false;
+                    break;
+                default:
+                    out.println("Opción no válida.");
+                    break;
+            }
+        }
+    }
+
+    private static void createProducts() {
+
+        try (Connection connection = DriverManager.getConnection(CONNECTION_STRING)) {
+            out.print("Ingrese el nombre: ");
+            String name = in.readLine();
+            out.print("Ingrese una descripcion: ");
+            String description = in.readLine();
+            out.print("Ingrese el precio del producto: ");
+            double price = Double.parseDouble(in.readLine());
+            out.print("Ingrese el numero del color del producto: ");
+            long color = Long.parseLong(in.readLine());
+            out.print("Ingrese el numero de talla del producto: ");
+            long size = Long.parseLong(in.readLine());
+            out.print("Ingrese el material del producto: ");
+            String material = in.readLine();
+            out.print("Ingrese el numero de categoria del producto: ");
+            long category = Long.parseLong(in.readLine());
+            out.print("Ingrese la cantidad deseada de productos: ");
+            long quantity = Long.parseLong(in.readLine());
+            out.print("Ingrese el Supply del producto: ");
+            long supplierid = Long.parseLong(in.readLine());
+
+            String storedProcedure = "{call CreateProductInventory(?, ?, ?, ?, ?, ?, ?, ?, ?)}";
+
+            try (CallableStatement callableStatement = connection.prepareCall(storedProcedure)) {
+
+                callableStatement.setString(1, name);
+                callableStatement.setString(2, description);
+                callableStatement.setBigDecimal(3, new java.math.BigDecimal(price));
+                callableStatement.setLong(4, color);
+                callableStatement.setLong(5, size);
+                callableStatement.setString(6, material);
+                callableStatement.setLong(7, category);
+                callableStatement.setLong(8, quantity);
+                callableStatement.setLong(9, supplierid);
+
+                boolean hasResults = callableStatement.execute();
+
+                if (hasResults == false) {
+                    out.println("El producto se creo y se agrego al inventario!!!");
+                }
+            }
+        } catch (Exception e) {
+            out.println("Error creando producto: " + e.getMessage());
+        }
+    }
+
+    private static void listProducts() {
+        try (Connection connection = DriverManager.getConnection(CONNECTION_STRING)) {
+            String query = "SELECT * FROM [Product]";
+            try (Statement stmt = connection.createStatement()) {
+                ResultSet rs = stmt.executeQuery(query);
+                while (rs.next()) {
+                    out.println("ID: " + rs.getLong("id") + ", Nombre: " + rs.getString("name") + ", Descripcion: " + rs.getString("descripcion") + ", Precio: " + rs.getDouble("price") + ", Color: " + rs.getLong("color") + ", Size: " + rs.getLong("size") + ", Material: " + rs.getString("material") + ", Caantidad: " + rs.getLong("quantity"));
+                }
+            }
+        } catch (Exception e) {
+            out.println("Error listando productos: " + e.getMessage());
+        }
+    }
+
+    private static void updateProducts() {
+
+        try (Connection connection = DriverManager.getConnection(CONNECTION_STRING)) {
+            out.print("Ingrese el ID del producto: ");
+            long id = Long.parseLong(in.readLine());
+            out.print("Ingrese el nombre: ");
+            String name = in.readLine();
+            out.print("Ingrese una descripcion: ");
+            String description = in.readLine();
+            out.print("Ingrese el precio del producto: ");
+            double price = Double.parseDouble(in.readLine());
+            out.print("Ingrese el numero del color del producto: ");
+            long color = Long.parseLong(in.readLine());
+            out.print("Ingrese el numero de talla del producto: ");
+            long size = Long.parseLong(in.readLine());
+            out.print("Ingrese el material del producto: ");
+            String material = in.readLine();
+            out.print("Ingrese el numero de categoria del producto: ");
+            long category = Long.parseLong(in.readLine());
+            out.print("Ingrese la cantidad deseada de productos: ");
+            long quantity = Long.parseLong(in.readLine());
+            out.print("Ingrese el Supply del producto: ");
+            long supplierid = Long.parseLong(in.readLine());
+
+            String storedProcedure = "{call UpdateProduct(?, ?, ?, ?, ?, ?, ?, ?, ?, ?)}";
+
+            try (CallableStatement callableStatement = connection.prepareCall(storedProcedure)) {
+
+                callableStatement.setLong(1, id);
+                callableStatement.setString(2, name);
+                callableStatement.setString(3, description);
+                callableStatement.setBigDecimal(4, new java.math.BigDecimal(price));
+                callableStatement.setLong(5, color);
+                callableStatement.setLong(6, size);
+                callableStatement.setString(7, material);
+                callableStatement.setLong(8, category);
+                callableStatement.setLong(9, quantity);
+                callableStatement.setLong(10, supplierid);
+
+                boolean hasResults = callableStatement.execute();
+
+                if (hasResults == false) {
+                    out.println("El producto se ha actualizado correctamente!!!");
+                }
+            }
+        } catch (Exception e) {
+            out.println("Error actualizando el producto: " + e.getMessage());
+        }
+    }
+
+    private static void deleteProducts() {
+
+        try (Connection connection = DriverManager.getConnection(CONNECTION_STRING)) {
+            out.print("Ingrese el ID del usuario a eliminar: ");
+            long id = Long.parseLong(in.readLine());
+
+            String query = "SELECT * FROM [Product] WHERE id = ?";
+            try (PreparedStatement ps = connection.prepareStatement(query)) {
+                ps.setLong(1, id);
+                ResultSet rs = ps.executeQuery();
+
+                if (rs.next()) {
+                    String deleteQuery = "DELETE FROM [Product] WHERE id = ?";
+                    try (PreparedStatement deleteStmt = connection.prepareStatement(deleteQuery)) {
+                        deleteStmt.setLong(1, id);
+                        deleteStmt.executeUpdate();
+                        out.println("Producto eliminado con éxito.");
+                    }
+                } else {
+                    out.println("Producto no encontrado.");
+                }
+            }
+        } catch (Exception e) {
+            out.println("Error eliminando producto: " + e.getMessage());
+        }
+    }
+
+    private static void wishlist(){
+        try (Connection connection = DriverManager.getConnection(CONNECTION_STRING)) {
+            out.print("Ingrese el ID del usuario: ");
+            long userid = Long.parseLong(in.readLine());
+            out.print("Ingrese el ID del producto: ");
+            long productid = Long.parseLong(in.readLine());
+
+
+            String storedProcedure = "{call CreateWishProduct(?, ?)}";
+
+            try (CallableStatement callableStatement = connection.prepareCall(storedProcedure)) {
+
+                callableStatement.setLong(1, userid);
+                callableStatement.setLong(2, productid);
+
+
+                boolean hasResults = callableStatement.execute();
+
+                if (hasResults == false) {
+                    out.println("El producto se agrego a su WishList!!!");
+                }
+            }
+        } catch (Exception e) {
+            out.println("Error creando Wishlist: " + e.getMessage());
+        }
+    }
+
+    private static void listarWishlist() {
+        try (Connection connection = DriverManager.getConnection(CONNECTION_STRING)) {
+            String query = "SELECT * FROM [Wishlist]";
+            try (Statement stmt = connection.createStatement()) {
+                ResultSet rs = stmt.executeQuery(query);
+                while (rs.next()) {
+                    out.println("ID: " + rs.getLong("id") + ", ID del usuario: " + rs.getLong("userid") + ", ID del producto: " + rs.getLong("productid") + ", Fecha: " + rs.getDate("dateAdded"));
+                }
+            }
+        } catch (Exception e) {
+            out.println("Error listando productos: " + e.getMessage());
+        }
+    }
+
+    private static void deleteWishlist() {
+
+        try (Connection connection = DriverManager.getConnection(CONNECTION_STRING)) {
+            out.print("Ingrese el ID del producto a eliminar de favoritos: ");
+            long id = Long.parseLong(in.readLine());
+
+            String query = "SELECT * FROM [Wishlist] WHERE productid = ?";
+            try (PreparedStatement ps = connection.prepareStatement(query)) {
+                ps.setLong(1, id);
+                ResultSet rs = ps.executeQuery();
+
+                if (rs.next()) {
+                    String deleteQuery = "DELETE FROM [Wishlist] WHERE productid = ?";
+                    try (PreparedStatement deleteStmt = connection.prepareStatement(deleteQuery)) {
+                        deleteStmt.setLong(1, id);
+                        deleteStmt.executeUpdate();
+                        out.println("Producto eliminado con éxito.");
+                    }
+                } else {
+                    out.println("Producto no encontrado.");
+                }
+            }
+        } catch (Exception e) {
+            out.println("Error eliminando producto: " + e.getMessage());
+        }
+    }
+
+    private static void manageOrders() throws IOException {
+        boolean managingOrders = true;
+
+        while (managingOrders) {
+            out.println("Gestión de Ordenes:");
+            out.println("1 - Crear Orden");
+            out.println("2 - Listar Ordenes");
+            out.println("3 - Actualizar Orden");
+            out.println("4 - Eliminar Orden");
+            out.println("5 - Regresar al menú anterior");
+
+            int choice = getUserChoice();
+            switch (choice) {
+                case 1:
+                    createOrder();
+                    break;
+                case 2:
+                    listOrders();
+                    break;
+                case 3:
+                    updateOrder();
+                    break;
+                case 4:
+                    deleteOrder();
+                    break;
+                case 5:
+                    managingOrders = false;
+                    break;
+                default:
+                    out.println("Opción no válida.");
+                    break;
+            }
+        }
+    }
+
+    private static void createOrder() {
+
+        try (Connection connection = DriverManager.getConnection(CONNECTION_STRING)) {
+            out.print("Ingrese el ID del usuario: ");
+            long userid = Long.parseLong(in.readLine());
+            out.print("Ingrese el ID del producto: ");
+            long productid = Long.parseLong(in.readLine());
+            out.print("Ingrese la cantidad a pedir: ");
+            long amount = Long.parseLong(in.readLine());
+            out.print("Ingrese el precio unitario del producto: ");
+            long unitprice = Long.parseLong(in.readLine());
+            out.print("Ingrese el estado de la orden: (pending, sent, delivered): ");
+            String state = in.readLine();
+
+            String storedProcedure = "{call CreateOrder(?, ?, ?, ?, ?)}";
+
+            try (CallableStatement callableStatement = connection.prepareCall(storedProcedure)) {
+
+                callableStatement.setLong(1, userid);
+                callableStatement.setLong(2, amount);
+                callableStatement.setString(3, state);
+                callableStatement.setLong(4, productid);
+                callableStatement.setLong(5, unitprice);
+
+
+                boolean hasResults = callableStatement.execute();
+
+                if (hasResults == false) {
+                    out.println("La orden se creo y se agrego a detalles de orden!!!");
+                }
+            }
+        } catch (Exception e) {
+            out.println("Error creando orden: " + e.getMessage());
+        }
+    }
+
+    private static void listOrders() {
+        try (Connection connection = DriverManager.getConnection(CONNECTION_STRING)) {
+            String query = "SELECT * FROM [Orders]";
+            try (Statement stmt = connection.createStatement()) {
+                ResultSet rs = stmt.executeQuery(query);
+                while (rs.next()) {
+                    out.println("ID: " + rs.getLong("id") + ", ID Usuario: " + rs.getLong("userId") + ", Fecha: " + rs.getDate("date") + ", Cantidad: " + rs.getInt("amount") + ", Estado: " + rs.getString("state"));
+                }
+            }
+        } catch (Exception e) {
+            out.println("Error listando ordenes: " + e.getMessage());
+        }
+    }
+
+    private static void updateOrder() {
+
+        try (Connection connection = DriverManager.getConnection(CONNECTION_STRING)) {
+            out.print("Ingrese el ID de la orden: ");
+            long id = Long.parseLong(in.readLine());
+            out.print("Ingrese el ID del usuario: ");
+            long userid = Long.parseLong(in.readLine());
+            out.print("Ingrese la cantidad de productos: ");
+            long amount = Long.parseLong(in.readLine());
+            out.print("Ingrese el estado de la orden: (pending, sent, delivered): ");
+            String state = in.readLine();
+            out.print("Ingrese el ID del producto: ");
+            long productid = Long.parseLong(in.readLine());
+            out.print("Ingrese el precio unitario del producto: ");
+            long unitprice = Long.parseLong(in.readLine());
+
+            String storedProcedure = "{call UpdateOrder(?, ?, ?, ?, ?, ?)}";
+
+            try (CallableStatement callableStatement = connection.prepareCall(storedProcedure)) {
+
+                callableStatement.setLong(1, id);
+                callableStatement.setLong(2, userid);
+                callableStatement.setLong(3, amount);
+                callableStatement.setString(4, state);
+                callableStatement.setLong(5, productid);
+                callableStatement.setLong(6, unitprice);
+
+                boolean hasResults = callableStatement.execute();
+
+                if (hasResults == false) {
+                    out.println("La orden se ha actualizado correctamente!!!");
+                }
+            }
+        } catch (Exception e) {
+            out.println("Error actualizando la orden: " + e.getMessage());
+        }
+    }
+
+    private static void deleteOrder() {
+
+        try (Connection connection = DriverManager.getConnection(CONNECTION_STRING)) {
+            out.print("Ingrese el ID de la orden a eliminar: ");
+            long id = Long.parseLong(in.readLine());
+
+            String query = "SELECT * FROM [Orders] WHERE id = ?";
+            try (PreparedStatement ps = connection.prepareStatement(query)) {
+                ps.setLong(1, id);
+                ResultSet rs = ps.executeQuery();
+
+                if (rs.next()) {
+                    String deleteQuery = "DELETE FROM [Orders] WHERE id = ?";
+                    try (PreparedStatement deleteStmt = connection.prepareStatement(deleteQuery)) {
+                        deleteStmt.setLong(1, id);
+                        deleteStmt.executeUpdate();
+                        out.println("Orden eliminada con éxito.");
+                    }
+                } else {
+                    out.println("Orden no encontrada.");
+                }
+            }
+        } catch (Exception e) {
+            out.println("Error eliminando orden: " + e.getMessage());
+        }
     }
 }
